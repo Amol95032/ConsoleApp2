a50c894d501a0046114d34bbe4a66f743662d031
Admin <Admin@DESKTOP-EPVV5M1>
2023-07-14 15:53:04 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,123 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+/*namespace ConsoleApp2
+{
+    internal class Program
+    {
+   
+       static void Main(string[] args)
+        {
+            int a = 5, b = 3;
+            a = a + b;
+            b = a - b;
+            a = a - b;
+        Console.WriteLine("print value of a ="+a);
+        Console.WriteLine("print value of b =" + b);
+
+
+            int i = 5, j = 3, k;
+            k = i;
+            i = j;
+            j = k;
+
+            Console.WriteLine("print value of i =" + i);
+            Console.WriteLine("print value of j =" + j);
+
+
+            int y, x = 2;
+            y = x++;
+            Console.WriteLine("value of y=" + y);
+            Console.WriteLine("value of x=" +x);
+
+            int M, N = 20;
+            M = ++N;
+            Console.WriteLine("value of M=" + M);
+            Console.WriteLine("value of N=" + N);
+        }
+    }
+}*/
+
+namespace ConsoleApp2
+{
+    internal class Arjun
+    {
+
+        static void Main(String[] args)
+
+        {
+            /* int Pin;
+             Pin = Convert.ToInt32(Console.ReadLine());
+             Console.WriteLine("Amol..");
+             if (Pin == 95032)
+             {
+                 Console.WriteLine("khirid wasti shivraj nagar Moshi");
+
+                 Console.WriteLine("9503209494");
+             }
+
+             /*int marks = 95;
+             if (marks >= 80 && marks <= 100)
+             {
+                 Console.WriteLine("student is Toprt");
+
+             }
+             else if (marks >= 60 && marks <= 80)
+             {
+                 Console.WriteLine("student is Frist");
+             }
+             else if (marks >= 50 && marks <= 60)
+             {
+                 Console.WriteLine("student is Second");
+             }
+             else if (marks >= 30 && marks <= 50)
+             {
+                 Console.WriteLine("student is Second");
+             }
+             else
+             {
+                 Console.WriteLine("student is faill");
+             }*/
+            /*write a program positive OR negative number*/
+            /* int num;
+             Console.WriteLine("please enter the num");
+             num=Convert.ToInt32(Console.ReadLine());
+             if(num>=0)
+             {
+               Console.WriteLine("Number is positive ="+num);
+             }
+            else
+            {
+               Console.WriteLine("Number is negative ="+num);
+            }*/
+            /* write a program odd or Even number */
+            /* int num;
+             Console.WriteLine("please enter the num");
+             num = Convert.ToInt32(Console.ReadLine());
+             if (num % 2 == 0)
+             {
+                 Console.WriteLine("Number is Even =" + num);
+             }
+             else
+             {
+                 Console.WriteLine("Number is Odd =" + num);
+             }*/
+            int num1, num2;
+            Console.WriteLine("Enter the First num");
+            num1 = Convert.ToInt32("Console.readline");
+            if (num1>num2)
+            {
+                Console.WriteLine("Enter the First largest Number="+num1);
+            }
+            else
+            {
+                Console.WriteLine("Enter the First Smalest Number=" + num2)
+            }
+            
+        
+        }
+    
+             
+    }
+                     
+}
+        
